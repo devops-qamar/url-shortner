b320c40ab3d01c87e898e3b96c009ccdd60552d4
Qamar Jahangir <qamar.dev04@gmail.com>
2026-07-28 14:39:49 +0500

    Add MongoDB configuration

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-const PORT = 3000;
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("🚀 URL Shortener API is Running");
+  res.send("🚀 URL Shortener API is Running");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
